0696a5465ed1b3c96909529a1d260e6923034da2
Fabian Maiwald <fabian.maiwald@cec.valantic.com>
2023-08-07 21:45:50 +0200

    first task

--- index.php
+++ index.php
@@ -7,68 +7,64 @@
 <body>
 <?php
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $name = $date = $description = $max = "";
 ?>
 
 <form action="" method="post">
 
-Name: <input type="text" name="name" value="<?php echo $name;?>"><br><br>
-Datum: <input type="date" name="date" value="<?php echo $date;?>"><br><br>
-Beschreibung: <textarea type="text" name="description" rows="5" cols="50" value="<?php echo $description;?>"></textarea><br><br>
-Maximale Anzahl Personen: <input type="number" name="max" value="<?php echo $max;?>"><br><br>
-Submit: <input type="submit" value="submit"><br>
+    Name: <input type="text" name="name" value="<?php echo $name; ?>"><br><br>
+    Datum: <input type="date" name="date" value="<?php echo $date; ?>"><br><br>
+    Beschreibung: <textarea type="text" name="description" rows="5" cols="50"
+                            value="<?php echo $description; ?>"></textarea><br><br>
+    Maximale Anzahl Personen: <input type="number" name="max" value="<?php echo $max; ?>"><br><br>
+    Submit: <input type="submit" value="submit"><br>
 
 </form>
 
 <?php
 
-if(isset($_POST['submit'])){
+$filename = '/home/fabianmaiwald/PhpstormProjects/EventPage/events.json';
+if (file_exists($filename)) {
+    $message = "Event Page Exists";
+} else {
+    $events = fopen($filename, 'w');
+}
 
-    $newEvent = array(
+if (isset($_POST["name"])) {
+    if ($_POST["name"] > 0) {
+        $newEvent = array(
             "name" => $_POST['name'],
             "date" => $_POST['date'],
             "description" => $_POST['description'],
             "max" => $_POST['max']
-    );
+        );
 
-if(filesize("events.json")==0){
-    $firstEvent = array($newEvent);
-    $safeEvent = $firstEvent;
-}else{
-    $oldEvent = json_decode(file_get_contents("events.json"));
-    array_push($oldEvent, $newEvent);
-    $safeEvent = $oldEvent;
-}
-
-$encoded_data = json_encode($safeEvent, JSON_PRETTY_PRINT);
-
-if(!file_put_contents("events.json", $encoded_data, LOCK_EX)){
-        $error = "Error storing Event, please try again";
-    }else{
-        $success = "Event is stored";
     }
+
+    $test = file_get_contents("events.json");
+    $oldEvent = json_decode($test, true);
+    $oldEvent[] = $newEvent;
+    $safeEvent = $oldEvent;
+
+
+    $encoded_data = json_encode($safeEvent, JSON_PRETTY_PRINT);
+    file_put_contents("events.json", $encoded_data);
+
+
+echo implode("<br>", $encoded_data);
+
+
+//var_dump($encoded_data);
+//    var_dump(json_decode($encoded_data, true));
+
 }
 ?>
 
 
-
-
-<?php
-//
-//echo "<br><br>";
-//echo htmlspecialchars($_POST["name"]);
-//echo "<br><br>";
-//echo "Datum: " . htmlspecialchars($_POST["date"]);
-//echo "<br><br>";
-//echo "Beschreibung: " . htmlspecialchars($_POST["description"]);
-//echo "<br><br>";
-//echo "Maximale Anzahl von Personen: " . htmlspecialchars($_POST["max"]);
-//
-//?>
-
-
 </body>
 </html>
-
-
 
