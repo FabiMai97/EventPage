09401c4cdd70c0f39e894331f989dd56cbabc221
Fabian Maiwald <fabian.maiwald@cec.valantic.com>
2023-08-11 11:40:47 +0200

    VAlidierungV2

--- index.php
+++ index.php
@@ -11,21 +11,48 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-
 ?>
-
 <form action="" method="post">
 
-    Name: <input type="text" name="name" value=""><br><br>
-    Datum: <input type="date" name="date" value=""><br><br>
-    Beschreibung: <textarea type="text" name="description" rows="5" cols="50" value=""></textarea><br><br>
-    Maximale Anzahl Personen: <input type="number" name="max" value=""><br><br>
-    Submit: <input type="submit" value="submit"><br><br>
+    <label> Name:
+        <input type="text" name="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>">
+        <?php if (isset($_POST['name']) && (strlen($_POST['name'])) <= 3) {
+            echo '<span style="color:orangered"> Error: Der Eventname muss mehr als 3 Zeichen haben! </span>';
+        } ?>
+    </label><br><br>
 
+
+    <label> Datum:
+        <input type="date" name="date" value="<?php echo isset($_POST['date']) ? $_POST['date'] : ''; ?>">
+        <?php if (isset($_POST['date']) && (strtotime($_POST['date'])) < time()) {
+            echo '<span style="color:orangered"> Error: Das Datum muss in der Zukunft liegen! </span>';
+        } ?>
+    </label><br><br>
+
+
+    <label>Beschreibung:
+        <textarea type="text" name="description" rows="5"
+                  cols="50"><?php echo isset($_POST['description']) ? $_POST['description'] : ''; ?></textarea>
+        <?php if (isset($_POST['description']) && (strlen($_POST['description'])) <= 5) {
+            echo '<span style="color:orangered"> Error: Die Eventbeschreibung muss mehr als 5 Zeichen haben! </span>';
+        } ?>
+    </label><br><br>
+
+
+    <label> Maximale Anzahl Personen:
+        <input type="number" name="max" value="<?php echo isset($_POST['max']) ? $_POST['max'] : ''; ?>" min="1">
+        <?php if (isset($_POST['max']) && (!is_numeric($_POST['max']) || $_POST['max'] < 0)) {
+            echo '<span style="color:orangered"> Error: "Maximale Anzahl von Personen" darf nicht negativ sein! </span';
+        } ?>
+    </label><br><br>
+
+
+    <label> Submit:
+        <input type="submit" value="submit"><br><br>
+    </label>
 </form>
 
 <?php
-
 
 $filename = '/home/fabianmaiwald/PhpstormProjects/EventPage/events.json';
 if (!file_exists($filename)) {
@@ -33,17 +60,14 @@ if (!file_exists($filename)) {
 }
 
 $oldData = file_get_contents("events.json");
-$old = json_decode($oldData, null, 512, 0);
+$old = json_decode($oldData, true, 512, 0);
 
-if (filesize($filename) !== 0) {
-    foreach ($old as $dol) {
-        echo $dol->name . "<br>";
-        echo $dol->date . "<br>";
-        echo $dol->description . "<br>";
-        echo $dol->max . "<br>";
-    }
+foreach ($old as $dol) {
+    echo $dol['name'] . "<br>";
+    echo $lod = date("d.m.y", strtotime($dol['date'])) . "<br>";
+    echo $dol['description'] . "<br>";
+    echo $dol['max'] . "<br>" . "<br>";
 }
-
 
 if (isset($_POST["name"])) {
     $newEvent = array(
@@ -52,22 +76,34 @@ if (isset($_POST["name"])) {
         "description" => $_POST['description'],
         "max" => $_POST['max']
     );
+    $error = [];
 
-    $test = file_get_contents("events.json");
-    $oldEvent = json_decode($test, true);
-    $oldEvent[] = $newEvent;
+    if (strlen($_POST['name']) <= 3) {
+        $error['name'] = "";
+    }
+    if (strtotime($_POST['date']) < time()) {
+        $error['date'] = "";
+    }
+    if (strlen($_POST['description']) <= 5) {
+        $error['description'] = "";
+    }
+    if (!is_numeric($_POST['max']) || $_POST['max'] < 0) {
+        $error['max'] = "";
+    }
 
-    $encodedData = json_encode($oldEvent, JSON_PRETTY_PRINT, 512);
-    file_put_contents("events.json", $encodedData);
+    if (empty($error)) {
+        $file = file_get_contents("events.json");
+        $oldEvent = json_decode($file, true, 521, 0);
+        $oldEvent [] = $newEvent;
 
+        $encodedData = json_encode($oldEvent, JSON_PRETTY_PRINT);
+        file_put_contents("events.json", $encodedData);
 
-
-
-    echo $_POST['name'] . "<br>";
-    echo $_POST['date'] . "<br>";
-    echo $_POST['description'] . "<br>";
-    echo $_POST['max'] . "<br>" . "<br>";
-
+        echo $_POST['name'] . "<br>";
+        echo date("d.m.y", strtotime($_POST['date'])) . "<br>";
+        echo $_POST['description'] . "<br>";
+        echo $_POST['max'] . "<br>" . "<br>";
+    }
 }
 ?>
 
